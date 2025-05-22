2339eb0be91b53ad701e5d659f871951d6d38e74
dokindokinpiano <dokindokinpiano@gmail.com>
2025-05-22 11:41:55 +0900

    PlyerMoveScript更新

--- Assets/Kato/PlayerMove.cs
+++ Assets/Kato/PlayerMove.cs
@@ -12,18 +12,22 @@ public class PlayerMove : MonoBehaviour
     private Rigidbody2D _rigidbody2D;
 
     [SerializeField]
-    private float PlayerMoveSpeed;
+    private float _playermovespeed;
 
     // Start is called before the first frame update
     void Start()
     {
+        _tr = transform;
         _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Input.GetKey(KeyCode.W);
-        
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector2 velocity = new Vector2(h, v);
+        _rb.velocity = velocity * _playermovespeed;
     }
 }
