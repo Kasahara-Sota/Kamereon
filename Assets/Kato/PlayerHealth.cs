89f0e587c6b9058c82833634a1b202518ecde377
SosukeKato <dokindokinpiano@gmail.com>
2025-05-22 17:28:16 +0900

    PlayerHealthScript更新

--- Assets/Kato/PlayerHealth.cs
+++ Assets/Kato/PlayerHealth.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(CapsuleCollider2D))]
 public class PlayerHealth : MonoBehaviour
 {
     public int health = 3;
