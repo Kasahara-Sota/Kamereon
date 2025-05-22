38c1ad0d53d7e57e9c8376db881d1cc847c9739a
kenkatou354 <kenkatou354@gmail.com>
2025-05-22 11:56:55 +0900

    ステージ切り替えを追加

--- Assets/Abe/TitleScript2.cs
+++ Assets/Abe/TitleScript2.cs
@@ -1,14 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class TitleScript : MonoBehaviour
+public class TitleScript2 : MonoBehaviour
+
 {
     public void ClickStartButton() //‰Ÿ‚µ‚½‚ç•ÏX
     {
         SceneManager.LoadScene("Title");
     }
 }
-
