96329eec2c25e445cf60d97a147f2a956166c0bf
KasaharaSota <souta.programer1201@gmail.com>
2025-05-23 13:11:54 +0900

    [update]プレイヤーの歩く音を再生できるように

--- Assets/Kato/PlayerMove.cs
+++ Assets/Kato/PlayerMove.cs
@@ -6,12 +6,14 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     private Rigidbody2D _rb;
-
+    private AudioSource _audioSource;
     [SerializeField] private float _playerMoveSpeed;
     void Start()
     {
         //Rigidbodyコンポーネントを取得
         _rb = GetComponent<Rigidbody2D>();
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.loop = true;
     }
     void Update()
     {
@@ -24,6 +26,14 @@ public class PlayerMove : MonoBehaviour
         if(h != 0 || v != 0)
         {
             transform.up = velocity;
+            if(!_audioSource.isPlaying)
+            {
+                _audioSource.Play();
+            }//ついでに歩く音を鳴らす
         }//入力があった時進行方向を向く
+        else
+        {
+            _audioSource.Stop();
+        }
     }
 }
